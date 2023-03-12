71a74ad47ecd75559f02bf9f6a92304949f25ee5
vadimshahray <vadimshahray@gmail.com>
2023-03-12 16:35:44 +0700

    feat: add Section

---
+++ src/components/Section.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { useState, PropsWithChildren } from 'react'
+import { ExpandLess, ExpandMore } from '@mui/icons-material'
+import {
+  Stack,
+  Tooltip,
+  Collapse,
+  Container,
+  IconButton,
+  Typography,
+} from '@mui/material'
+
+export type SectionProps = {
+  title: string
+  Icon?: Icon
+  collapsible?: boolean
+} & PropsWithChildren
+
+export const Section = ({
+  title,
+  Icon,
+  children,
+  collapsible,
+}: SectionProps) => {
+  const [expanded, setExpanded] = useState(true)
+
+  const handleClick = () => {
+    setExpanded(!expanded)
+  }
+
+  return (
+    <Container>
+      <Stack direction='row' alignItems='center' justifyContent='space-between'>
+        <Typography variant='subtitle1' display='flex' alignItems='center'>
+          {Icon && <Icon sx={{ marginRight: 1 }} />}
+          {title}
+        </Typography>
+
+        {collapsible && (
+          <Tooltip title={expanded ? 'Свернуть' : 'Развернуть'}>
+            <IconButton onClick={handleClick} size='small'>
+              {expanded ? <ExpandLess /> : <ExpandMore />}
+            </IconButton>
+          </Tooltip>
+        )}
+      </Stack>
+
+      <Collapse in={expanded}>{children}</Collapse>
+    </Container>
+  )
+}
--- src/pages/MainPage/InfoSidebar/InfoSidebar.tsx
+++ src/pages/MainPage/InfoSidebar/InfoSidebar.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
-import { Container, Typography } from '@mui/material'
+import { Section } from '@components'
+import { Container } from '@mui/material'
 
 export const InfoSidebar = () => {
   return (
     <Container>
-      <Typography textAlign='center'>Информационная панель</Typography>
+      <Section title='Моторчик' collapsible></Section>
+      <Section title='Катушка' collapsible></Section>
+      <Section title='Вольтметр' collapsible></Section>
+      <Section title='Катушки Гельмгольца' collapsible></Section>
     </Container>
   )
 }
