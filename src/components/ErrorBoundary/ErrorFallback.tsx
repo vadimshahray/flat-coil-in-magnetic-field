a55c2841f5abe5304acbd23e00d4eac6fe1866bd
vadimshahray <vadimshahray@gmail.com>
2023-03-19 00:00:34 +0700

    style: ErrorFallback

--- src/components/ErrorBoundary/ErrorFallback.tsx
+++ src/components/ErrorBoundary/ErrorFallback.tsx
@@ -1,7 +1,7 @@
 import React, { ErrorInfo } from 'react'
 import { useTranslate } from '@languages'
+import { Box, Stack, Typography } from '@mui/material'
 import { ErrorOutlineOutlined } from '@mui/icons-material'
-import { Box, Paper, Stack, Typography } from '@mui/material'
 
 type Props = {
   error?: Error
@@ -15,35 +15,32 @@ export const ErrorFallback = ({ error, errorInfo }: Props) => {
     <Box
       sx={{
         width: '100%',
-        height: '100%',
         display: 'flex',
-        alignItems: 'center',
         justifyContent: 'center',
+        padding: 4,
       }}
     >
-      <Paper sx={{ width: 600, padding: 4 }}>
-        <Stack spacing={1}>
-          <Stack direction='row' spacing={1} alignItems='center'>
-            <ErrorOutlineOutlined color='error' fontSize='large' />
+      <Stack spacing={1} sx={{ width: 720 }}>
+        <Stack direction='row' spacing={1} alignItems='center'>
+          <ErrorOutlineOutlined color='error' fontSize='large' />
 
-            <Typography variant='h4' color='error'>
-              {translate('title')}
-            </Typography>
-          </Stack>
-
-          <Typography variant='h6' color='error'>
-            {error?.name}
-          </Typography>
-
-          <Typography variant='body1' color='error'>
-            {error?.message}
-          </Typography>
-
-          <Typography variant='caption' color='error' whiteSpace='pre-line'>
-            {errorInfo?.componentStack}
+          <Typography variant='h4' color='error'>
+            {translate('title')}
           </Typography>
         </Stack>
-      </Paper>
+
+        <Typography variant='h6' color='error'>
+          {error?.name}
+        </Typography>
+
+        <Typography variant='body1' color='error'>
+          {error?.message}
+        </Typography>
+
+        <Typography variant='caption' color='error' whiteSpace='pre-line'>
+          {errorInfo?.componentStack}
+        </Typography>
+      </Stack>
     </Box>
   )
 }
