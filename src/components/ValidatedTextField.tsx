56e5bcf7b7c9da4e7890a19d2efccd890c5101ec
vadimshahray <vadimshahray@gmail.com>
2023-03-18 23:09:48 +0700

    feat: add ValidatedTextField

---
+++ src/components/ValidatedTextField.tsx
@@ -0,0 +1,106 @@
+import { useDispatch } from '@hooks'
+import { InputAdornment, TextField, Tooltip } from '@mui/material'
+import React, { useEffect, useMemo, useState, useRef } from 'react'
+import { AnyObject, object, ValidationError, NumberSchema } from 'yup'
+import {
+  increaseInvalidFieldsNumber,
+  decreaseInvalidFieldsNumber,
+} from '@slices'
+
+/** Пропсы `ValidatedTextField` */
+export type ValidatedTextFieldProps = {
+  /** Ярлык поля */
+  label: string
+  value?: string
+  /** Числовое правило валидации */
+  rule: NumberSchema<number, AnyObject>
+  /**
+   * Колбэк, вызываемый, когда пользовательский ввод валиден
+   * @param {number} value Валидное значение
+   */
+  onValid(value: number): unknown
+
+  /** Приписка к полю с левой стороны */
+  adornment?: string
+
+  endIconTooltip?: string
+  EndIcon?: Icon
+}
+
+/**
+ * Автоматически валидируемое текстовое поле
+ * @returns {JSX.Element}
+ */
+export const ValidatedTextField = ({
+  label,
+  rule,
+  adornment,
+  value,
+  onValid,
+  endIconTooltip,
+  EndIcon,
+}: ValidatedTextFieldProps) => {
+  const dispatch = useDispatch()
+
+  const [error, setError] = useState<string>()
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  const schema = useMemo(
+    () =>
+      object({
+        value: rule,
+      }),
+    [rule],
+  )
+
+  const handleChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = e.target.value
+
+    schema
+      .validate({
+        value: newValue,
+      })
+      .then(({ value }) => {
+        onValid(value)
+        setError(undefined)
+      })
+      .catch((error: ValidationError) => {
+        setError(error.message)
+      })
+  }
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.value = value ?? ''
+    }
+
+    setError(undefined)
+  }, [value])
+
+  useEffect(() => {
+    if (error) dispatch(increaseInvalidFieldsNumber())
+    else dispatch(decreaseInvalidFieldsNumber())
+  }, [error, dispatch])
+
+  return (
+    <TextField
+      inputRef={inputRef}
+      label={label}
+      error={!!error}
+      helperText={error}
+      onChange={handleChange}
+      InputProps={{
+        startAdornment: adornment && (
+          <InputAdornment position='start'>{adornment}</InputAdornment>
+        ),
+        endAdornment: EndIcon && endIconTooltip && (
+          <InputAdornment position='end'>
+            <Tooltip title={endIconTooltip}>
+              <EndIcon fontSize='small' />
+            </Tooltip>
+          </InputAdornment>
+        ),
+      }}
+    />
+  )
+}
