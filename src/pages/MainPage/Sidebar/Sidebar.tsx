987b2b83b339e0f04c29f3ab77e977cc67088d5f
vadimshahray <vadimshahray@gmail.com>
2023-03-19 13:10:36 +0700

    feat: toggle Sidebar content by modeling status

--- src/pages/MainPage/Sidebar/Sidebar.tsx
+++ src/pages/MainPage/Sidebar/Sidebar.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import { Box } from '@mui/material'
+import { useSelector } from 'react-redux'
+import { ModelingInfo } from './ModelingInfo'
 import { Configuration } from './Configuration'
+import { selectModelingStatus } from '@selectors'
 
 export const Sidebar = () => {
+  const modelingStatus = useSelector(selectModelingStatus)
+
   return (
     <Box sx={{ overflowY: 'auto' }}>
-      <Configuration />
+      {modelingStatus === 'idle' ? <Configuration /> : <ModelingInfo />}
     </Box>
   )
 }
