816946096cb350b6f1aae68c4d32e8dae3ac2573
vadimshahray <vadimshahray@gmail.com>
2023-03-19 16:36:19 +0700

    refactor: import document directly

--- src/components/ProjectDocumentButton.tsx
+++ src/components/ProjectDocumentButton.tsx
@@ -2,17 +2,14 @@ import React from 'react'
 import { useTranslate } from '@languages'
 import { IconButton, Tooltip } from '@mui/material'
 import { TextSnippetOutlined } from '@mui/icons-material'
+import ProjectDocumentPath from '../assets/alternator.pdf'
 
 export const ProjectDocumentButton = () => {
   const translate = useTranslate('ProjectDocumentButton')
 
   return (
     <Tooltip title={translate('downloadDocument')}>
-      <a
-        href={process.env.PUBLIC_URL + '/docs/alternator.pdf'}
-        download
-        style={{ textDecoration: 'none' }}
-      >
+      <a href={ProjectDocumentPath} download style={{ textDecoration: 'none' }}>
         <IconButton>
           <TextSnippetOutlined />
         </IconButton>
