cd938859a4f7f2f6c9f93b072a4dd8cecc003190
vadimshahray <vadimshahray@gmail.com>
2023-03-27 14:06:48 +0700

    feat: rotate Coil on modeling

--- src/models/Coil/Coil.tsx
+++ src/models/Coil/Coil.tsx
@@ -1,8 +1,11 @@
-import React from 'react'
 import * as THREE from 'three'
+import { store } from 'src/store'
 import { GLTF } from 'three-stdlib'
+import React, { useRef } from 'react'
 import CoilModelPath from './coil.glb'
 import { useGLTF } from '@react-three/drei'
+import { useFrame } from '@react-three/fiber'
+import { selectModelingStatus, selectMotorSpeed } from '@selectors'
 
 type GLTFResult = GLTF & {
   nodes: {
@@ -17,6 +20,19 @@ type GLTFResult = GLTF & {
 const Coil = (props: JSX.IntrinsicElements['group']) => {
   const { nodes, materials } = useGLTF(CoilModelPath) as GLTFResult
 
+  const ref = useRef<THREE.Mesh>(null)
+
+  useFrame(() => {
+    if (!ref.current) return
+
+    if (selectModelingStatus(store.getState()) === 'idle')
+      return (ref.current.rotation.x = Math.PI / 2)
+
+    if (selectModelingStatus(store.getState()) !== 'started') return
+
+    ref.current.rotateX(selectMotorSpeed(store.getState()) / 10)
+  })
+
   return (
     <group {...props} dispose={null} scale={110}>
       <mesh
@@ -26,9 +42,11 @@ const Coil = (props: JSX.IntrinsicElements['group']) => {
       />
 
       <mesh
+        ref={ref}
         geometry={nodes.Cube001.geometry}
         material={nodes.Cube001.material}
         position={[0, 0.27, 0]}
+        rotation={[Math.PI / 2, 0, 0]}
         scale={[0.69, 0.23, 0.01]}
       />
     </group>
