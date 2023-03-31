4435751866d77362ff30351579051d023386b0a2
vadimshahray <vadimshahray@gmail.com>
2023-03-31 21:38:53 +0700

    feat: add State to Scene

--- src/pages/MainPage/Scene/Scene.tsx
+++ src/pages/MainPage/Scene/Scene.tsx
@@ -3,11 +3,14 @@ import React, { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
 import { DeviceTable } from './DeviceTable'
 import { SceneFallback } from './SceneFallback'
+import { Stats } from '@react-three/drei'
 
 export const Scene = () => {
   return (
     <Suspense fallback={<SceneFallback />}>
       <Canvas resize={{ debounce: 0 }}>
+        <Stats />
+
         <Camera />
 
         <directionalLight intensity={0.3} position={[300, 600, 200]} />
