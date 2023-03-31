36009d8fed6262628331d42782d3fd9637b10a06
vadimshahray <vadimshahray@gmail.com>
2023-03-31 21:40:14 +0700

    feat: move some translate to controls.dict

--- src/languages/dictionary/controls.dict.ts
+++ src/languages/dictionary/controls.dict.ts
@@ -10,6 +10,11 @@ export const controlsRU = {
   DisassembleSchemeButton: {
     disassembleScheme: 'Разобрать схему',
   },
+
+  ChangeSidebarVisibilityButton: {
+    hideSidebar: 'Скрыть информационную панель',
+    showSidebar: 'Показать информационную панель',
+  },
 }
 
 export const controlsEN = {
@@ -23,5 +28,10 @@ export const controlsEN = {
 
   DisassembleSchemeButton: {
     disassembleScheme: 'Disassemble the scheme',
+  },
+
+  ChangeSidebarVisibilityButton: {
+    hideSidebar: 'Hide information sidebar',
+    showSidebar: 'Show information sidebar',
   },
 }
--- src/languages/dictionary/modelingScene.dict.ts
+++ src/languages/dictionary/modelingScene.dict.ts
@@ -2,20 +2,10 @@ export const modelingSceneRU = {
   SceneFallback: {
     modelsLoading: 'Загрузка моделей...',
   },
-
-  ChangeSidebarVisibilityButton: {
-    hideSidebar: 'Скрыть информационную панель',
-    showSidebar: 'Показать информационную панель',
-  },
 }
 
 export const modelingSceneEN = {
   SceneFallback: {
     modelsLoading: 'Models loading...',
-  },
-
-  ChangeSidebarVisibilityButton: {
-    hideSidebar: 'Hide information sidebar',
-    showSidebar: 'Show information sidebar',
   },
 }
