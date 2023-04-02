727cdf5908ce0b16f04475090924e55b39e89971
vadimshahray <vadimshahray@gmail.com>
2023-04-03 01:26:50 +0700

    feat: add Engine model

--- src/constants/scene.ts
+++ src/constants/scene.ts
@@ -7,16 +7,21 @@ export const CAMERA_ALL_DEVICES_POSITION = CAMERA_DEFAULT_POSITION.clone().add(
 export const CAMERA_SCHEME_ASSEMBLY_POSITION =
   CAMERA_DEFAULT_POSITION.clone().add(new THREE.Vector3(0, 700, 0))
 
-export const COIL_SCENE_POSITION = new THREE.Vector3(0, 44, -110)
+export const ENGINE_SCENE_POSITION = new THREE.Vector3(400, 0, -80)
+export const CAMERA_ENGINE_POSITION = ENGINE_SCENE_POSITION.clone().add(
+  new THREE.Vector3(0, 20, 120),
+)
+
+export const COIL_SCENE_POSITION = new THREE.Vector3(0, 44, -80)
 export const CAMERA_COIL_POSITION = COIL_SCENE_POSITION.clone().add(
   new THREE.Vector3(0, 20, 120),
 )
 
-export const VOLTMETER_SCENE_POSITION = new THREE.Vector3(170, 19, 40)
+export const VOLTMETER_SCENE_POSITION = new THREE.Vector3(340, 19, 140)
 export const CAMERA_VOLTMETER_POSITION = VOLTMETER_SCENE_POSITION.clone().add(
   new THREE.Vector3(0, 20, 120),
 )
 
-export const HELMHOLTZ_COILS_SCENE_POSITION = new THREE.Vector3(-240, 185, 70)
+export const HELMHOLTZ_COILS_SCENE_POSITION = new THREE.Vector3(0, 185, -80)
 export const CAMERA_HELMHOLTZ_COILS_POSITION =
   HELMHOLTZ_COILS_SCENE_POSITION.clone().add(new THREE.Vector3(0, 60, 220))
--- src/pages/MainPage/Scene/DeviceTable.tsx
+++ src/pages/MainPage/Scene/DeviceTable.tsx
@@ -3,12 +3,14 @@ import { WireHeap } from './WireHeap'
 import { VoltmeterCoilPlusWire } from './VoltmeterCoilPlusWire'
 import {
   COIL_SCENE_POSITION,
+  ENGINE_SCENE_POSITION,
   VOLTMETER_SCENE_POSITION,
   HELMHOLTZ_COILS_SCENE_POSITION,
 } from '@constants'
 
 const Coil = React.lazy(() => import('src/models/Coil'))
 const Table = React.lazy(() => import('src/models/Table'))
+const Engine = React.lazy(() => import('src/models/Engine'))
 const Voltmeter = React.lazy(() => import('src/models/Voltmeter'))
 const HelmholtzCoils = React.lazy(() => import('src/models/HelmholtzCoils'))
 
@@ -24,6 +26,8 @@ export const DeviceTable = () => {
       <VoltmeterCoilPlusWire />
 
       <Voltmeter position={VOLTMETER_SCENE_POSITION} />
+
+      <Engine position={ENGINE_SCENE_POSITION} />
 
       <WireHeap />
     </>
