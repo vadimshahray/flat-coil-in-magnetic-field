868fe78a96f9b4d7671754895849b81f71d0dbf2
vadimshahray <vadimshahray@gmail.com>
2023-04-15 15:10:35 +0700

    feat: change AmperageGenerator model

--- src/models/AmperageGenerator/AmperageGenerator.tsx
+++ src/models/AmperageGenerator/AmperageGenerator.tsx
@@ -3,29 +3,29 @@ import * as THREE from 'three'
 import { GLTF } from 'three-stdlib'
 import { useGLTF } from '@react-three/drei'
 import AmperageGeneratorModelPath from './amperageGenerator.glb'
+import { TerminalConnectingZone } from '@components'
 
 type GLTFResult = GLTF & {
   nodes: {
-    counter_1: THREE.Mesh
-    counter_2: THREE.Mesh
-    counter_3: THREE.Mesh
-    counter_4: THREE.Mesh
-    counter_5: THREE.Mesh
-    counter_6: THREE.Mesh
-    counter_7: THREE.Mesh
-    counter_8: THREE.Mesh
-    counter_9: THREE.Mesh
+    Cube003: THREE.Mesh
+    Cube003_1: THREE.Mesh
+    Cube003_2: THREE.Mesh
+    Cube003_3: THREE.Mesh
+    Cube003_4: THREE.Mesh
+    Cube003_5: THREE.Mesh
+    Cube003_6: THREE.Mesh
+    Cylinder: THREE.Mesh
+    power: THREE.Mesh
   }
   materials: {
-    grey_3: THREE.MeshStandardMaterial
-    grey_2: THREE.MeshStandardMaterial
+    tabl_background: THREE.MeshStandardMaterial
     black: THREE.MeshStandardMaterial
     white: THREE.MeshStandardMaterial
-    grey_4: THREE.MeshStandardMaterial
+    backgrond: THREE.MeshStandardMaterial
+    very_gray: THREE.MeshStandardMaterial
     red: THREE.MeshStandardMaterial
-    grey: THREE.MeshStandardMaterial
-    yellow: THREE.MeshStandardMaterial
-    green: THREE.MeshStandardMaterial
+    blue: THREE.MeshStandardMaterial
+    power: THREE.MeshStandardMaterial
   }
 }
 
@@ -33,18 +33,57 @@ const AmperageGenerator = (props: JSX.IntrinsicElements['group']) => {
   const { nodes, materials } = useGLTF(AmperageGeneratorModelPath) as GLTFResult
 
   return (
-    <group {...props} dispose={null} scale={3} rotation={[0, -Math.PI / 2, 0]}>
-      <group scale={[0.57, 0.24, 0.48]}>
-        <mesh geometry={nodes.counter_1.geometry} material={materials.grey_3} />
-        <mesh geometry={nodes.counter_2.geometry} material={materials.grey_2} />
-        <mesh geometry={nodes.counter_3.geometry} material={materials.black} />
-        <mesh geometry={nodes.counter_4.geometry} material={materials.white} />
-        <mesh geometry={nodes.counter_5.geometry} material={materials.grey_4} />
-        <mesh geometry={nodes.counter_6.geometry} material={materials.red} />
-        <mesh geometry={nodes.counter_7.geometry} material={materials.grey} />
-        <mesh geometry={nodes.counter_8.geometry} material={materials.yellow} />
-        <mesh geometry={nodes.counter_9.geometry} material={materials.green} />
+    <group rotation={[0, -Math.PI / 2, 0]} {...props} dispose={null}>
+      <group scale={0.6}>
+        <group position={[97.4, -47.89, 33.75]} scale={[4.65, 35, 54.2]}>
+          <mesh
+            geometry={nodes.Cube003.geometry}
+            material={materials.tabl_background}
+          />
+          <mesh
+            geometry={nodes.Cube003_1.geometry}
+            material={materials.black}
+          />
+          <mesh
+            geometry={nodes.Cube003_2.geometry}
+            material={materials.white}
+          />
+          <mesh
+            geometry={nodes.Cube003_3.geometry}
+            material={materials.backgrond}
+          />
+          <mesh
+            geometry={nodes.Cube003_4.geometry}
+            material={materials.very_gray}
+          />
+          <mesh geometry={nodes.Cube003_5.geometry} material={materials.red} />
+          <mesh geometry={nodes.Cube003_6.geometry} material={materials.blue} />
+        </group>
+
+        <mesh
+          geometry={nodes.Cylinder.geometry}
+          material={nodes.Cylinder.material}
+          position={[110.03, 38.69, -68.62]}
+          rotation={[Math.PI / 2, 0, -Math.PI / 2]}
+          scale={[-19.1, -8.98, -19.1]}
+        />
+
+        <mesh
+          geometry={nodes.power.geometry}
+          material={materials.power}
+          position={[105.13, -47.2, -68.62]}
+          scale={[3.92, 5.58, 5.58]}
+        />
       </group>
+
+      <TerminalConnectingZone
+        terminal='AmperageGenerator+'
+        position={new THREE.Vector3(70, -22, 3)}
+      />
+      <TerminalConnectingZone
+        terminal='AmperageGenerator-'
+        position={new THREE.Vector3(70, -22, 40)}
+      />
     </group>
   )
 }
