438b607ca6a6e2f8cdcbbdf4dd0e808404e1d38a
vadimshahray <vadimshahray@gmail.com>
2023-04-18 21:05:46 +0700

    feat: add Sink to Room

--- src/pages/MainPage/Scene/Room/Room.tsx
+++ src/pages/MainPage/Scene/Room/Room.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { RoomBox } from './RoomBox'
-import { FLOOR_W } from '@constants'
+import { FLOOR_W, WALL_H } from '@constants'
 
 const Door = React.lazy(() => import('src/models/Door'))
+const Sink = React.lazy(() => import('src/models/Sink'))
 
 export const Room = () => {
   return (
@@ -13,6 +14,8 @@ export const Room = () => {
         position={[1.8 * FLOOR_W, 0, -2 * FLOOR_W]}
         rotation={[0, Math.PI, 0]}
       />
+
+      <Sink position={[2.85 * FLOOR_W, 0.24 * WALL_H, -1.9 * FLOOR_W]} />
     </>
   )
 }
