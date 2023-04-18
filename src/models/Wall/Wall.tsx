6ef23b61e01bdb601bf7a397b96f21757c97cc4b
vadimshahray <vadimshahray@gmail.com>
2023-04-18 19:08:57 +0700

    feat: change Wall

---
+++ src/models/Wall/Wall.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { useTexture } from '@react-three/drei'
+import WallAOPath from './textures/wall_ao.jpg'
+import WallColorPath from './textures/wall_color.jpg'
+import WallHeightPath from './textures/wall_height.jpg'
+import WallNormalPath from './textures/wall_normal.jpg'
+import WallRoughnessPath from './textures/wall_roughness.jpg'
+
+const size = 1024
+
+const Wall = (props: JSX.IntrinsicElements['group']) => {
+  const textureMaps = useTexture({
+    aoMap: WallAOPath,
+    map: WallColorPath,
+    normalMap: WallNormalPath,
+    roughnessMap: WallRoughnessPath,
+    displacementMap: WallHeightPath,
+  })
+
+  return (
+    <group {...props} dispose={null}>
+      <mesh position={[0, size / 2, 0]}>
+        <boxGeometry args={[1, size, size]} />
+
+        <meshStandardMaterial {...textureMaps} />
+      </mesh>
+    </group>
+  )
+}
+
+export default Wall
