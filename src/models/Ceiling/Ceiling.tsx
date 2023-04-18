11c762a0dc6f75b5f14aa1caeafbf56f7e884803
vadimshahray <vadimshahray@gmail.com>
2023-04-18 18:28:28 +0700

    feat: add Ceiling

---
+++ src/models/Ceiling/Ceiling.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { useTexture } from '@react-three/drei'
+import ConcreteAOPath from './textures/concrete_ao.jpg'
+import ConcreteHeightPath from './textures/concrete_height.jpg'
+import ConcreteNormalPath from './textures/concrete_normal.jpg'
+import ConcreteDiffusePath from './textures/concrete_diffuse.jpg'
+import ConcreteMetallicPath from './textures/concrete_metallic.jpg'
+import ConcreteRoughnessPath from './textures/concrete_roughness.jpg'
+
+const size = 1024
+
+const Ceiling = (props: JSX.IntrinsicElements['group']) => {
+  const textureMaps = useTexture({
+    aoMap: ConcreteAOPath,
+    map: ConcreteDiffusePath,
+    normalMap: ConcreteNormalPath,
+    metalnessMap: ConcreteMetallicPath,
+    roughnessMap: ConcreteRoughnessPath,
+    displacementMap: ConcreteHeightPath,
+  })
+
+  return (
+    <group {...props} dispose={null}>
+      <mesh position={[0, size / 2, 0]} rotation={[0, Math.PI / 2, 0]}>
+        <boxGeometry args={[1, size, size]} />
+
+        <meshStandardMaterial {...textureMaps} />
+      </mesh>
+    </group>
+  )
+}
+
+export default Ceiling
