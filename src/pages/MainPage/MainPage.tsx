ce0dbae220e78f73c1d121a8c8df41a48e872e72
vadimshahray <vadimshahray@gmail.com>
2023-04-20 16:48:05 +0700

    feat: auto assembly scheme on mobile devices

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,12 +1,22 @@
-import React from 'react'
 import { Scene } from './Scene'
 import { Sidebar } from './Sidebar'
+import { useDispatch } from '@hooks'
 import { Controls } from './Controls'
+import React, { useEffect } from 'react'
+import { assemblyScheme } from '@slices'
 import { useMediaQuery, useTheme } from '@mui/material'
 
 export const MainPage = () => {
+  const dispatch = useDispatch()
+
   const theme = useTheme()
   const isDesktop = useMediaQuery(theme.breakpoints.up('sm'))
+
+  useEffect(() => {
+    if (!isDesktop) {
+      dispatch(assemblyScheme())
+    }
+  }, [isDesktop, dispatch])
 
   return isDesktop ? (
     <div
