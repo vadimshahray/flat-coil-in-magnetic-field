6bf67c64e618a075f0193c43e3bf529f0b6a110b
vadimshahray <vadimshahray@gmail.com>
2023-04-21 20:37:52 +0700

    feat: add textures for window

--- src/models/Window/Window.tsx
+++ src/models/Window/Window.tsx
@@ -10,7 +10,10 @@ import React from 'react'
 import * as THREE from 'three'
 import { GLTF } from 'three-stdlib'
 import WindowModelPath from './window.glb'
-import { useGLTF } from '@react-three/drei'
+import { useGLTF, useTexture } from '@react-three/drei'
+import WindowColorTexture from './textures/window_color.png'
+import WindowNormalTexture from './textures/window_normal.png'
+import WindowRoughnessTexture from './textures/window_roughness.png'
 
 type GLTFResult = GLTF & {
   nodes: {
@@ -24,60 +27,41 @@ type GLTFResult = GLTF & {
     Plane_1_PBR_0: THREE.Mesh
     Plane_PBR_0: THREE.Mesh
   }
-  materials: {
-    ['PBR.1']: THREE.MeshStandardMaterial
-    Disc__0: THREE.MeshStandardMaterial
-    material: THREE.MeshStandardMaterial
-    ['PBR.2']: THREE.MeshStandardMaterial
-  }
 }
 
 const Window = (props: JSX.IntrinsicElements['group']) => {
-  const { nodes, materials } = useGLTF(WindowModelPath) as GLTFResult
+  const { nodes } = useGLTF(WindowModelPath) as GLTFResult
+  const maps = useTexture({
+    map: WindowColorTexture,
+    normalMap: WindowNormalTexture,
+    roughnessMap: WindowRoughnessTexture,
+  })
+
+  const material = new THREE.MeshStandardMaterial(maps)
 
   return (
     <group {...props} dispose={null}>
       <group scale={2} position={[0, -65, 0]} rotation={[0, Math.PI, 0]}>
-        <group
-          position={[294.55, 746.44, -1513.97]}
-          rotation={[-Math.PI, -1.39, 2.85]}
-        />
-
         <group position={[37.2, 333.26, -45.45]} rotation={[0, 0, -1.59]}>
-          <mesh
-            geometry={nodes.Disc__0.geometry}
-            material={materials.Disc__0}
-          />
+          <mesh geometry={nodes.Disc__0.geometry} material={material} />
         </group>
         <group position={[99.24, 334.61, -12.87]} rotation={[-1.6, 0, 0]}>
-          <mesh
-            geometry={nodes.Tube_PBR_0.geometry}
-            material={materials.material}
-          />
+          <mesh geometry={nodes.Tube_PBR_0.geometry} material={material} />
         </group>
         <group position={[-121.57, 538.83, 0]}>
           <mesh
             geometry={nodes.Cylinder_1_PBR2_0.geometry}
-            material={materials['PBR.2']}
+            material={material}
           />
         </group>
         <group position={[-121.57, 64.99, 0]}>
-          <mesh
-            geometry={nodes.Cylinder_PBR2_0.geometry}
-            material={materials['PBR.2']}
-          />
+          <mesh geometry={nodes.Cylinder_PBR2_0.geometry} material={material} />
         </group>
         <group position={[100.7, 309.82, -11.53]} rotation={[-1.57, 0, 0]}>
-          <mesh
-            geometry={nodes.Plane_1_PBR_0.geometry}
-            material={materials.material}
-          />
+          <mesh geometry={nodes.Plane_1_PBR_0.geometry} material={material} />
         </group>
         <group position={[-354.68, 296.8, 0]} rotation={[1.57, 0, 0]}>
-          <mesh
-            geometry={nodes.Plane_PBR_0.geometry}
-            material={materials.material}
-          />
+          <mesh geometry={nodes.Plane_PBR_0.geometry} material={material} />
         </group>
       </group>
     </group>
