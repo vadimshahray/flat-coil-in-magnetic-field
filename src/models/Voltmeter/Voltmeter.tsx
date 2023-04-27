0e21b99434a0e636d29c52c9b39d12e20827f4e9
vadimshahray <vadimshahray@gmail.com>
2023-04-27 15:49:41 +0700

    feat: change Voltmeter model

--- src/models/Voltmeter/Voltmeter.tsx
+++ src/models/Voltmeter/Voltmeter.tsx
@@ -8,15 +8,20 @@ import VoltmeterModelPath from './voltmeter.glb'
 import { Text, useGLTF } from '@react-three/drei'
 import { TerminalConnectingZone } from '@components'
 import { selectIsVoltmeterConnected, selectModelingVoltage } from '@selectors'
-
 type GLTFResult = GLTF & {
   nodes: {
     Cube_1: THREE.Mesh
     Cube_2: THREE.Mesh
+    Cube_3: THREE.Mesh
+    Cube_4: THREE.Mesh
+    Cube_5: THREE.Mesh
   }
   materials: {
     ['Material.001']: THREE.MeshStandardMaterial
     ['Material.002']: THREE.MeshStandardMaterial
+    blue: THREE.MeshStandardMaterial
+    very_grey_plastic: THREE.MeshPhysicalMaterial
+    red: THREE.MeshStandardMaterial
   }
 }
 
@@ -43,20 +48,22 @@ const Voltmeter = (props: JSX.IntrinsicElements['group']) => {
 
   return (
     <group {...props} dispose={null}>
-      <group position={[0, 26, 0]}>
-        <group
-          scale={[20, 25, 50]}
-          rotation={[0, THREE.MathUtils.degToRad(-90), 0]}
-        >
+      <group position={[0, 24.49, 0]}>
+        <group rotation={[0, -Math.PI / 2, 0]} scale={[20, 25, 50]}>
           <mesh
             geometry={nodes.Cube_1.geometry}
             material={materials['Material.001']}
           />
-
           <mesh
             geometry={nodes.Cube_2.geometry}
             material={materials['Material.002']}
           />
+          <mesh geometry={nodes.Cube_3.geometry} material={materials.blue} />
+          <mesh
+            geometry={nodes.Cube_4.geometry}
+            material={materials.very_grey_plastic}
+          />
+          <mesh geometry={nodes.Cube_5.geometry} material={materials.red} />
         </group>
 
         <TerminalConnectingZone
