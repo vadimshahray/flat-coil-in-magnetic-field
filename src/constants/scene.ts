f5d65c51381ed2389a2ec8cd5413fef414e4aeb7
vadimshahray <vadimshahray@gmail.com>
2023-05-03 20:28:29 +0700

    feat: move all table devices

--- src/constants/scene.ts
+++ src/constants/scene.ts
@@ -7,17 +7,17 @@ export const CAMERA_ALL_DEVICES_POSITION = CAMERA_DEFAULT_POSITION.clone().add(
   new THREE.Vector3(0, 430, 540),
 )
 
-export const ENGINE_SCENE_POSITION = new THREE.Vector3(400, TABLE_TOP, -74)
+export const ENGINE_SCENE_POSITION = new THREE.Vector3(400, TABLE_TOP, -10)
 export const CAMERA_ENGINE_POSITION = ENGINE_SCENE_POSITION.clone().add(
   new THREE.Vector3(0, 20, 120),
 )
 
-export const COIL_SCENE_POSITION = new THREE.Vector3(0, TABLE_TOP, -80)
+export const COIL_SCENE_POSITION = new THREE.Vector3(0, TABLE_TOP, -16)
 export const CAMERA_COIL_POSITION = COIL_SCENE_POSITION.clone().add(
   new THREE.Vector3(0, 20, 120),
 )
 
-export const VOLTMETER_SCENE_POSITION = new THREE.Vector3(240, TABLE_TOP, 190)
+export const VOLTMETER_SCENE_POSITION = new THREE.Vector3(240, TABLE_TOP, 200)
 export const CAMERA_VOLTMETER_POSITION = VOLTMETER_SCENE_POSITION.clone().add(
   new THREE.Vector3(0, 20, 120),
 )
@@ -25,7 +25,7 @@ export const CAMERA_VOLTMETER_POSITION = VOLTMETER_SCENE_POSITION.clone().add(
 export const HELMHOLTZ_COILS_SCENE_POSITION = new THREE.Vector3(
   0,
   TABLE_TOP,
-  -80,
+  -16,
 )
 export const CAMERA_HELMHOLTZ_COILS_POSITION =
   HELMHOLTZ_COILS_SCENE_POSITION.clone().add(new THREE.Vector3(0, 60, 220))
@@ -33,7 +33,7 @@ export const CAMERA_HELMHOLTZ_COILS_POSITION =
 export const CURRENT_SOURCE_SCENE_POSITION = new THREE.Vector3(
   -420,
   TABLE_TOP + 34,
-  -85,
+  -21,
 )
 export const CAMERA_CURRENT_SOURCE_POSITION =
   CURRENT_SOURCE_SCENE_POSITION.clone().add(
@@ -43,7 +43,7 @@ export const CAMERA_CURRENT_SOURCE_POSITION =
 export const OSCILLOSCOPE_SCENE_POSITION = new THREE.Vector3(
   -421,
   TABLE_TOP + 67,
-  -60,
+  4,
 )
 export const CAMERA_OSCILLOSCOPE_POSITION =
   VOLTMETER_SCENE_POSITION.clone().add(new THREE.Vector3(0, 20, 120))
