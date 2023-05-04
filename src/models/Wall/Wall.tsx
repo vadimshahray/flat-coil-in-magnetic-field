90d6b89fe9c9220e173cc07ba8740afc97d0b424
vadimshahray <vadimshahray@gmail.com>
2023-05-05 00:02:01 +0700

    feat: change Wall texture

--- src/models/Wall/Wall.tsx
+++ src/models/Wall/Wall.tsx
@@ -1,19 +1,11 @@
 import React from 'react'
 import { WALL_H, WALL_D } from '@constants'
 import { useTexture } from '@react-three/drei'
-import WallAOPath from './textures/wall_ao.jpg'
-import WallColorPath from './textures/wall_color.jpg'
-import WallHeightPath from './textures/wall_height.jpg'
-import WallNormalPath from './textures/wall_normal.jpg'
-import WallRoughnessPath from './textures/wall_roughness.jpg'
+import WallColorPath from './textures/map.jpg'
 
 const Wall = (props: JSX.IntrinsicElements['group']) => {
   const textureMaps = useTexture({
-    aoMap: WallAOPath,
     map: WallColorPath,
-    normalMap: WallNormalPath,
-    roughnessMap: WallRoughnessPath,
-    displacementMap: WallHeightPath,
   })
 
   return (
