dded2b0d53a71b4653031be0981f3a571bc653d5
vadimshahray <vadimshahray@gmail.com>
2023-05-07 13:18:54 +0700

    ff

--- src/constants/wire.ts
+++ src/constants/wire.ts
@@ -1,14 +1,12 @@
-import * as THREE from 'three'
-
 export const WIRES_LENGTH = 0.2
 export const WIRES_SECTION = 0.5 * 10 ** -6
 export const WIRES_RESISTIVITY = 2.8 * 10 ** -8
 export const WIRES_MATERIAL = 'Al'
 
-export const DISCONNECTED_WIRE_POINTS = [
-  new THREE.Vector3(0, 0, 0),
-  new THREE.Vector3(30, 0, 3),
-  new THREE.Vector3(120, 0, -2),
-  new THREE.Vector3(160, 0, 3),
-  new THREE.Vector3(260, 0, 0),
+export const DISCONNECTED_WIRE_POINTS: [number, number, number][] = [
+  [0, 0, 0],
+  [30, 0, 3],
+  [120, 0, -2],
+  [160, 0, 3],
+  [260, 0, 0],
 ]
