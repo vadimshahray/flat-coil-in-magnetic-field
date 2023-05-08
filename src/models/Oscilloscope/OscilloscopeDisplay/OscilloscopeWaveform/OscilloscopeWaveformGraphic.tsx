21b024f1a234dfc115e2c02dbb3ddea285adbf16
vadimshahray <vadimshahray@gmail.com>
2023-05-08 14:39:18 +0700

    feat: scale OscilloscopeGraphic by params

--- src/models/Oscilloscope/OscilloscopeDisplay/OscilloscopeWaveform/OscilloscopeWaveformGraphic.tsx
+++ src/models/Oscilloscope/OscilloscopeDisplay/OscilloscopeWaveform/OscilloscopeWaveformGraphic.tsx
@@ -7,6 +7,8 @@ import {
 } from '@constants'
 import {
   selectModelingVoltageMax,
+  selectOscilloscopeScaleY,
+  selectOscilloscopeScaleX,
   selectEngineRotationFrequency,
   selectIsOscilloscopeConnected,
 } from '@selectors'
@@ -15,14 +17,19 @@ type Props = {
   size: number
 }
 const T_MAX = 1 / ENGINE_ROTATION_FREQUENCY_MIN
-
 const T_MAX_SIZE = T_MAX * GRID_CELLS_COUNT_IN_QUARTER
 
 export const OscilloscopeWaveformGraphic = ({ size }: Props) => {
   const isConnected = useSelector(selectIsOscilloscopeConnected)
 
-  const V = useSelector(selectModelingVoltageMax)
-  const T = 1 / useSelector(selectEngineRotationFrequency)
+  const V = scaleV(
+    useSelector(selectModelingVoltageMax),
+    useSelector(selectOscilloscopeScaleY),
+  )
+  const T = scaleT(
+    1 / useSelector(selectEngineRotationFrequency),
+    useSelector(selectOscilloscopeScaleX),
+  )
 
   if (!isConnected) return null
 
@@ -58,4 +65,18 @@ export const OscilloscopeWaveformGraphic = ({ size }: Props) => {
   }
 
   return <group>{oscillationsInQuarter.map(renderOscillation)}</group>
+}
+
+const T_SCALE_INIT = 0.25
+const scaleT = (T: number, scale: number) => {
+  const k = T_SCALE_INIT / scale
+
+  return T * k
+}
+
+const V_SCALE_INIT = 0.12875
+const scaleV = (V: number, scale: number) => {
+  const k = V_SCALE_INIT / scale
+
+  return V * k
 }
